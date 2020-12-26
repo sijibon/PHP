b52e1e3d08c1b8fed17bd2da1993122aa09db352
jibon shohidul <you@example.com>
2020-12-27 02:22:38 +0600

    factorial

---
+++ factorial.php
@@ -0,0 +1,21 @@
+<?php
+
+$n = 5;
+
+for($i = $n, $factorial = 1; $i > 0; $i-- ){
+    $factorial = $factorial * $i;
+}
+
+echo $factorial;
+
+echo "<br>";
+//factorial of 20
+
+
+$number = 20;
+
+for($i = $number, $factorial = 1; $i > 0 ; $i--){
+    $factorial = $factorial * $i;
+}
+
+echo $factorial;
