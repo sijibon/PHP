942d2c83e8bb601ed67c2b753a4c39001d0a6816
sijibon <shohidulislam382298@.com>
2021-01-20 22:42:07 +0600

    list_function

---
+++ array/list_function.php
@@ -0,0 +1,14 @@
+<?php
+
+$array = ['jibon','joy','Imam','Al-amin'];
+
+ $new_array =  list($a, $b, $c, $d) = $array;
+
+echo $a." ";
+echo $b." ";
+echo $c." ";
+echo $d." ";
+
+ echo "<pre>";
+print_r($new_array);
+echo "</pre>";
