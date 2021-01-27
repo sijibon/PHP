5e862f5e3e08b4bc29e548553989df284a296201
sijibon <shohidulislam382298@.com>
2021-01-27 21:31:43 +0600

    addslashes/stripslashes

--- string/addslash_stripslash.php
+++ string/addslash_stripslash.php
@@ -13,3 +13,25 @@ echo "<br>";
 $str = addslashes('What does "yolo" mean?');
 
 echo $str;
+echo "<br>";
+echo stripslashes($str);
+echo "<br>";
+
+
+$name = "shohidul islam";
+
+echo addcslashes($name, "s");
+echo "<br>";
+echo stripslashes($name);
+
+
+$name = "shohidul islam DFGHJ";
+echo $name;
+echo "<br>";
+
+$newStr =  addcslashes($name, "a..s");
+
+echo $newStr;
+
+
+
