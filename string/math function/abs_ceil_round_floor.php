394c5ccb7a42ac792a16860dd7868c844dfbda63
sijibon <shohidulislam382298@.com>
2021-01-28 11:09:49 +0600

    math function floor/ceil/round/abs

---
+++ string/math function/abs_ceil_round_floor.php
@@ -0,0 +1,31 @@
+<?php
+
+$num = -30.457;
+echo "The negative number = ".$num."<br>";
+echo "after used abs function = ".abs($num);
+echo "<br>";
+
+
+$num = 30.457;
+echo "The value is = ".$num;
+echo "<br>";
+echo "after used ceil function = ".ceil($num);
+echo "<br>";
+
+
+$num = 30.457;
+echo "The value is = ".$num;
+echo "<br>";
+echo "after used floor function = ".floor($num);
+echo "<br>";
+
+
+
+$num = 30.857;
+echo "The value is = ".$num;
+echo "<br>";
+echo "after used round function = ".round($num);
+
+
+
+
