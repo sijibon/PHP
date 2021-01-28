9d187adf4f9041999c2c71c715e62de8fee79193
sijibon <shohidulislam382298@.com>
2021-01-28 11:28:55 +0600

    rand functions

---
+++ string/math function/intDiv_sqrt_pow.php
@@ -0,0 +1,17 @@
+<?php
+//intDiv
+//sqrt
+//pow
+
+echo intdiv(10,2);
+echo "<br>";
+echo intdiv(-20,-2);
+echo "<br>";
+
+
+//sqrt
+ echo sqrt(9)."<br>";
+ echo sqrt(-9)."<br>";
+
+ //pow
+ echo pow(3,3)."<br>";
---
+++ string/math function/rand_mt_rand_lcg_value.php
@@ -0,0 +1,12 @@
+<?php
+//rand()
+ echo rand(1,6)."<br>";
+ echo rand()."<br>";
+ //mt_rand()
+ echo mt_rand(1,10)."<br>";
+ echo mt_rand()."<br>";
+
+
+ //lcg_value()
+ echo lcg_value();
+
