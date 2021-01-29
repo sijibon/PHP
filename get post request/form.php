420d082203063d5bbf49730b811afa0cb86320a5
sijibon <shohidulislam382298@.com>
2021-01-30 02:25:03 +0600

    get/post/request

---
+++ get post request/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input form</title>
+</head>
+<body>
+    <form action="test_form.php" method="get">
+        <label for="">Name</label>
+         <input name="name" type="text" placeholder="Name">
+        <br> <br>
+         <label for="">Gender</label>
+         <input name="gender" type="text" placeholder="Gender"><br>
+         <button name="save" type="submit">save</button>
+    </form>
+</body>
+</html>
---
+++ get post request/test_form.php
@@ -0,0 +1,19 @@
+<?php
+// echo "<pre>";
+// print_r($_GET);
+// echo "<pre>";
+
+// echo $_GET['name']."<br>";
+// echo $_GET['gender']."<br>";
+
+
+
+
+echo "<pre>";
+print_r($_REQUEST);
+echo "<pre>";
+
+echo $_REQUEST['name']."<br>";
+echo $_REQUEST['gender'];
+
+?>
